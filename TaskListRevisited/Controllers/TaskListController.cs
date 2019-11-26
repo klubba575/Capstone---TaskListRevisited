078d1be30899d9c666de90d8a7d0664149a86f6a
klubba575 <wilderandrew9@gmail.com>
2019-11-25 22:56:18 -0500

    Task List Revisited Incomplete Upload

---
+++ TaskListRevisited/Controllers/TaskListController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TaskListRevisited.Models;
+
+namespace TaskListRevisited.Controllers
+{
+	
+    public class TaskListController : Controller
+    {
+		private readonly TaskListContext _context;
+
+		public TaskListController(TaskListContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+        {
+            return View();
+        }
+		public IActionResult ListTasks()
+		{
+			var listofTasks = _context.Tasks.ToList();
+			return View(listofTasks);
+		}
+		[HttpGet]
+		public IActionResult AddNewTask()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult AddNewTask(Tasks newtask)
+		{
+			if(ModelState.IsValid)
+			{
+				_context.Tasks.Add(newtask);
+				_context.SaveChanges();
+			}
+			return RedirectToAction("Index");
+		}
+    }
+}
---
+++ TaskListRevisited/Models/Tasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskListRevisited.Models
+{
+    public partial class Tasks
+    {
+        public int TaskId { get; set; }
+        public string Details { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool? Completion { get; set; }
+        public string UserId { get; set; }
+        public string Name { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
